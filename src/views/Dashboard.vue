c5a85794264a5f4be9ddee0a2dcde8f6803f2b9b
lutfialfiansyah <lutfialfiansyah28@gmail.com>
2019-04-10 05:41:04 +0700

    topbar

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="dashboard">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">Home</li>
+        <li class="breadcrumb-item active">Dashboard</li>
+      </ol>
+      <h2 style="text-align:center;">Back Office Agency</h2>
+      <!-- <div class="container-fluid">
+        <div class="animated fadeIn">
+          <div class="row">
+            <div class="col-sm-6 col-lg-3">
+              <div class="card text-white bg-primary">
+                <div class="card-body pb-0">
+                  <div class="btn-group float-right">
+                    <button type="button" class="btn btn-transparent dropdown-toggle p-0" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="icon-settings"></i>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right">
+                    </div>
+                  </div>
+                  <h4 class="mb-0">2</h4>
+                  <p>Members Staff</p>
+                </div>
+                <div class="chart-wrapper px-3" style="height:70px;">
+                  <canvas id="card-chart1" class="chart" height="70"></canvas>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+              <div class="card text-white bg-info">
+                <div class="card-body pb-0">
+                  <button type="button" class="btn btn-transparent p-0 float-right">
+                    <i class="icon-location-pin"></i>
+                  </button>
+                  <h4 class="mb-0">3</h4>
+                  <p>Data Apply</p>
+                </div>
+                <div class="chart-wrapper px-3" style="height:70px;">
+                  <canvas id="card-chart2" class="chart" height="70"></canvas>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+              <div class="card text-white bg-warning">
+                <div class="card-body pb-0">
+                  <div class="btn-group float-right">
+                    <button type="button" class="btn btn-transparent dropdown-toggle p-0" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="icon-settings"></i>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right">
+
+                    </div>
+                  </div>
+                  <h4 class="mb-0">4</h4>
+                  <p>Data Promo</p>
+                </div>
+                <div class="chart-wrapper" style="height:70px;">
+                  <canvas id="card-chart3" class="chart" height="70"></canvas>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+              <div class="card text-white bg-danger">
+                <div class="card-body pb-0">
+                  <div class="btn-group float-right">
+                    <button type="button" class="btn btn-transparent dropdown-toggle p-0" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="icon-settings"></i>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right">
+
+                    </div>
+                  </div>
+                  <h4 class="mb-0">5</h4>
+                  <p>Data Merchant</p>
+                </div>
+                <div class="chart-wrapper px-3" style="height:70px;">
+                  <canvas id="card-chart4" class="chart" height="70"></canvas>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid">
+        <div class="animated fadeIn">
+          <div class="card">
+            <div class="card-header">
+              Halaman Dashboard Administrator
+            </div>
+            <div class="card-body" style="padding: 20px">
+              <p>Traditional heading elements are designed to work best in the meat of your page content. When you need a heading to stand out, consider using a
+                <strong>display heading</strong>—a larger, slightly more opinionated heading style.</p>
+              <div class="bd-example">
+                <p>
+                  <small>This line of text is meant to be treated as fine print.</small>
+                </p>
+                <p>
+                  <strong>This line rendered as bold text.</strong>
+                </p>
+                <p>
+                  <em>This line rendered as italicized text.</em>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> -->
+  </div>
+</template>
+<script>
+//   export default {
+//     name: 'Dashboard',
+//     mounted() {
+//     if ( ! this.$session.get('token')) {
+//       this.$router.push({name: 'Login'})
+//     }
+//   }
+//   }
+</script>
