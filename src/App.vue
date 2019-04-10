f94db931cf84a8f02961a67701cec9d01d6fd85e
adenjimmy <nbhnazfar@gmail.com>
2019-04-10 10:05:44 +0700

    First 3

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="app">
+        <topbar-nav></topbar-nav>
+
     <router-view/>
   </div>
 </template>
+<script>
+  import TopbarNav from './components/template/topbar.vue'
+  // import SidebarNav from './components/template/sidebar.vue'
+  // import FooterNav from './components/template/footer.vue'
+
+export default {
+  name: 'App',
+  components: { TopbarNav }
+}
+</script>
---
+++ src/components/template/topbar.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="topnav">
+    <!-- <div class="relative_box">
+      <img alt="Logout logo" style="height:30px;width35px;" src="../../assets/img/logout.png">
+            <h3><span>Maler<br />und<br />Tapezierarbeiten</span></h3>
+        <a class="active" href="#home">Home</a>
+    </div>     -->
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+  <a href="#about">About</a>
+</div>
+
+</template>
+
+<script>
+//     export default {
+//         data: function () {
+//     return {
+//       name: null
+//     }},
+//     mounted() {
+//       if (this.$session.has('token')) {
+
+//           this.$router.push('/')
+          
+//       }
+//       this.name = this.$session.get('name')
+//   },
+//     }
+</script>
