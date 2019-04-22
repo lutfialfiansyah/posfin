aa28c627b7c4c3bdd37e1c3ca932b05535a80271
adenjimmy <nbhnazfar@gmail.com>
2019-04-22 12:29:19 +0700

    Update 5.5

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -39,20 +39,6 @@
                 <p>Bangsistek © 2011, SOPPPOS Development Team, All Right Reserved.</p>
             </div>
         </div>
-        <div class="container">
-            <div class="footer">
-                <div class="online">
-                    <p><img class="icon-footer" src="../assets/images/outline-language-24-px@3x.png"> ONLINE</p>
-                </div>
-                <div class="ip"> <p>IP Server: 10.33.41.48   Port: 5775</p></div>
-                <div class="akun">
-                    <p><img class="icon-footer" src="../assets/images/outline-account-circle-24-px@3x.png"> 1001 SIDIK PRAMONO 331000086 ADMIN AGENSI</p>
-                </div>
-                <div class="date">
-                    <p>31/12/2019 12:00:00 AM</p>
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 
